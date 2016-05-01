f0e8995afcbac0023054d6129f316f3ce99d84ee
Yaoyu Yang <yaoyu.yang88@gmail.com>
2016-05-01 16:36:06 -0700

    working version

--- src/js/app.js
+++ src/js/app.js
@@ -2,35 +2,110 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 var ajax = require('ajax');
 var Accel = require('ui/accel');
+var Settings = require('settings');
 
-var main = new UI.Card({
-  title: 'HookPebble',
-  icon: 'images/menu_icon.png',
-  body: 'Welcome to Hook for Pebble!',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+Settings.config(
+  { url: 'http://www.yaoyuyang.com/hookautomation/config/' },
+  // open
+  function(e) {
+    console.log('opening configurable');
+  },
+  // close
+  function(e) {
+    console.log('closed configurable');
+    // Show the parsed response
+    console.log(JSON.stringify(e.options));
+  }
+);
+
+
+var controlCard = function(item, urlUp, urlDown) {
+  var card = new UI.Card({
+    title: item.title,
+    action: {
+      up: 'images/on.png',
+      down: 'images/off.png',
+      select: 'images/shake.png',
+      backgroundColor: 'black'
+    },
+    body: "Shake to toggle enabled!",
+    toggleStatus: true
+  });
+  card.show();
+  card.on('click', 'up', function() {
+    hookRequest(urlUp);
+    console.log('Up clicked!');
+  });
+  card.on('click', 'down', function() {
+    hookRequest(urlDown);
+    console.log('Down clicked!');
+  });
+  card.on('click', 'select', function() {
+    console.log('Select clicked!');
+    if (card.toggleStatus) {
+      card.body("Shake to toggle disabled!");
+      card.toggleStatus = false;
+    } else {
+      card.body("Shake to toggle enabled!");
+      card.toggleStatus = true;
+    }
+  });
+  card.on('click', 'back', function() {
+    card.hide();
+  });
+  var toggle = 0;
+  Accel.init();
+  Accel.on('tap', function(e) {
+    console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
+    if (card.toggleStatus) {
+      if (toggle === 0) {
+        hookRequest(urlUp);
+        toggle = 1;
+      } else if (toggle === 1) {
+        hookRequest(urlDown);
+        toggle = 0;
+      }
+    }
+  });
+};
+
+var menu = new UI.Menu({
+  backgroundColor: 'white',
+  textColor: 'black',
+  highlightBackgroundColor: 'bulgarianRose',
+  highlightTextColor: 'white',
+  sections: [{
+    title: 'HookPebble',
+    items: [{
+      title: 'First Device',
+      subtitle: 'ON or OFF'
+    }, {
+      title: 'Second Device',
+      subtitle: 'ON or OFF'
+    }]
+  }]
+});
+
+menu.show();
+
+menu.on('select', function(e) {
+  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+  console.log('The item is titled "' + e.item.title + '"');
+  controlCard(e.item, urlOn, urlOff);
 });
 // Prepare the accelerometer
 var toggle = 0;
-Accel.init();
-Accel.on('tap', function(e) {
-  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
-  if (toggle === 0) {
-    turnON();
-    toggle = 1;
-  } else if (toggle === 1) {
-    turnOFF();
-    toggle = 0;
-  }
-});
 
-var turnOFF = function() {
+var urlOff = 'https://api.gethook.io/v1/device/trigger/dae116066f8f3641ed086c89c2621a02/Off/?token=389230d7ca6a6adf4c7baf2bb4c62042';
+var urlOn = 'https://api.gethook.io/v1/device/trigger/dae116066f8f3641ed086c89c2621a02/On/?token=389230d7ca6a6adf4c7baf2bb4c62042';
+
+var hookRequest = function(url) {
   ajax(
     {
-      url: 'https://api.gethook.io/v1/device/trigger/dae116066f8f3641ed086c89c2621a02/Off/?token=389230d7ca6a6adf4c7baf2bb4c62042',
+      url: url,
     },
     function(data, status, request) {
-      console.log('Turned OFF');
+      console.log('Request sent');
       console.log(data);
     },
     function(error, status, request) {
@@ -38,20 +113,3 @@ var turnOFF = function() {
     }
   );
 }
-
-var turnON = function() {
-  ajax(
-    {
-      url: 'https://api.gethook.io/v1/device/trigger/dae116066f8f3641ed086c89c2621a02/On/?token=389230d7ca6a6adf4c7baf2bb4c62042',
-    },
-    function(data, status, request) {
-      console.log('Turned ON');
-      console.log(data);
-    },
-    function(error, status, request) {
-      console.log('No!!!');
-    }
-  );
-}
-
-main.show();
